ca254f2a3f823b3985efb7becee8421c8f169f84
junolym <junolym@163.com>
2017-04-24 00:11:08 +0800

    修复cookie无法自动失效的错误

--- plugins/cookie-manager.js
+++ plugins/cookie-manager.js
@@ -3,7 +3,7 @@ CookieManager = {
     add : function(cookie, username) {
         this.cookies[cookie] = username;
         setTimeout(function() {
-            del(cookie);
+            CookieManager.del(cookie);
         }, 6*60*60*1000); // 6 hours
     },
     check : function(cookie) {
