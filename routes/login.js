af418cdbc9fc58e9da1890feaacd0225fb6becb8
ChenxRin <1104358312@qq.com>
2017-04-25 19:05:59 +0800

    Add files via upload
    login数据库改为mysql

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var dao = require('./../models/DAO.js');
+var dao = require('./../dao/dao.js');
 var cm = require('../plugins/cookie-manager.js');
-var User = require('./../models/User.js');
 
 router.get('/', function(req, res, next) {
     if (req.cookies && cm.check(req.cookies.id)) {
@@ -13,14 +12,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res) {
-    dao.getuserbyname(req.body.form_username, function(err, doc) {
-        if(err) {
-            res.render('error', err);
-        } else if(!doc) {
-            res.render('login', { error : '用户不存在' });
-        } else if(doc && doc.password != req.body.form_password) {
-            res.render('login', { error : '密码错误' });
-        } else {
+    dao.login(req.body.form_username, req.body.form_password, function(err, doc) {
+        if (!err) {
             require('crypto').randomBytes(16, function(ex, buf) {
                 var token = buf.toString('hex');
                 console.log(token);
@@ -28,6 +21,10 @@ router.post('/', function(req, res) {
                 res.redirect('/');
                 cm.add(token, req.body.form_username);
             });
+        } else if(err == 2) {
+            res.render('login', { error : '密码错误' });
+        } else {
+            res.render('login', { error : '用户不存在' });
         }
     });
 });
