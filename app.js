86884434073a82b83ae5d3fe721c328efb934466
junolym <junolym@163.com>
2017-04-29 01:49:13 +0800

    完善课程和签到列表的功能

--- app.js
+++ app.js
@@ -46,11 +46,12 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error =  err;
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  console.log(err);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,54 +2,54 @@ var express = require('express');
 var cm = require('../plugins/cookie-manager.js');
 var router = express.Router();
 var dao = require('../dao/dao.js');
+
 router.get('/', function(req, res, next) {
-  if (req.cookies && cm.check(req.cookies.id)) {
-    res.render('index', { title: 'Classhelper', user: cm.getCookie(req.cookies.id) });
-  } else {
-    res.redirect('/login');
-  }
+    if (req.cookies && cm.check(req.cookies.id)) {
+        res.render('index', { title: 'Classhelper', user: cm.getCookie(req.cookies.id) });
+    } else {
+        res.redirect('/login');
+    }
 });
 
 router.get('/index/course', function(req, res, next) {
-    var courses = [];
     if (req.cookies && cm.check(req.cookies.id)) {
-        dao.getcoursebyaccount(cm.getCookie(req.cookies.id), function(err, result){
-            if (err) {
-                console.log(err);
+        dao.getcoursebyaccount(cm.getCookie(req.cookies.id), function(err, result) {
+            if (!err) {
+                var courses = JSON.parse(JSON.stringify(result));
+                res.render('content/index-course', { title: '课程列表', courses: courses });
+            } else {
+                res.render('error', { error: err });
             }
-            else if (!result) {
-                console.log("result为空");
-            }
-            else {
-            var json = JSON.parse(JSON.stringify(result));
-            res.render('content/index-course', { title: '课程列表', courses: json });
-            }
-        })
+        });
+    } else {
+        res.redirect('/login');
     }
-    
-    
 });
 
 router.get('/index/signin', function(req, res, next) {
-    var signin = [];
     if (req.cookies && cm.check(req.cookies.id)) {
-        dao.getsignbyaccount(cm.getCookie(req.cookies.id), function(err, result){
-            if (err) {
-                console.log(err);
-            }
-            else if (!result) {
-                console.log("result为空");
-            }
-            else {
-            var json = JSON.parse(JSON.stringify(result));
-            res.render('content/index-signin', { title: '签到列表', signin: json });
+        dao.getsignbyaccount(cm.getCookie(req.cookies.id), function(err, result) {
+            if (!err) {
+                var signin = JSON.parse(JSON.stringify(result));
+                signin.forEach(function(s) {
+                    s.time = (new Date(s.time)).toLocaleString();
+                });
+                res.render('content/index-signin', { title: '签到列表', signin: signin });
+            } else {
+                res.render('error', { error: err });
             }
         })
+    } else {
+        res.redirect('/login');
     }
 });
 
 router.get('/index/exam', function(req, res, next) {
-    res.render('content/index-exam', { title: '测验列表' });
+    if (req.cookies && cm.check(req.cookies.id)) {
+        res.render('content/index-exam', { title: '测验列表' });
+    } else {
+        res.redirect('/login');
+    }
 });
 
 module.exports = router;
