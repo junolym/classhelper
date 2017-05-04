9e9b5991ed45c01ad5a1db1e4ea28bd2d9be6d95
ChenxRin <1104358312@qq.com>
2017-05-04 23:24:58 +0800

    学生名单修改

--- routes/home.js
+++ routes/home.js
@@ -3,13 +3,14 @@ var cm = require('../plugins/cookie-manager.js');
 var url = require('url');
 var router = express.Router();
 var crypto = require('crypto');
+require('json-tryparse');
 var dao = require('../dao/dao.js');
 
 router.get('/course', function(req, res, next) {
     if (req.cookies && cm.check(req.cookies.id)) {
         dao.getcoursebyaccount(cm.getCookie(req.cookies.id), function(err, result) {
             if (!err) {
-                var courses = JSON.parse(JSON.stringify(result));
+                var courses = JSON.tryParse(JSON.stringify(result));
                 res.render('home/course', { title: '课程列表', courses: courses });
             } else {
                 res.render('error', { error: err });
@@ -24,7 +25,7 @@ router.get('/signin', function(req, res, next) {
     if (req.cookies && cm.check(req.cookies.id)) {
         dao.getsignbyaccount(cm.getCookie(req.cookies.id), function(err, result) {
             if (!err) {
-                var signin = JSON.parse(JSON.stringify(result));
+                var signin = JSON.tryParse(JSON.stringify(result));
                 signin.forEach(function(s) {
                     s.time = (new Date(s.time)).toLocaleString('zh-CN', { hour12 : false })
                         .replace(/[\/|-]/, '年').replace(/[\/|-]/, '月').replace(/ /, '日 ');
@@ -57,9 +58,19 @@ router.get('/addcourse', function(req, res, next) {
 
 router.post('/addcourse', function(req, res, next) {
     if (req.cookies && cm.check(req.cookies.id)) {
+        var params = url.parse(req.url, true).query;
         dao.addcourse(cm.getCookie(req.cookies.id), req.body.form_coursename, req.body.form_coursetime, req.body.form_courseinfo, function(err, result) {
             if (!err) {
-                res.render('home/reload', { location : 'course' });
+                
+                dao.addstutocourse(result, JSON.tryParse(req.body.students), function(err, result){
+                    if (!err) {
+                        res.render('home/reload', { location : 'course' });
+                    }
+                    else {
+                        res.render('error', { error : err });
+                    }
+                });
+
             } else {
                 res.render('error', { error : err });
             }
@@ -77,7 +88,7 @@ router.get('/signindetail', function(req, res, next) {
             if (!err) {
                 dao.getsignbyid(params.sid, function(err, result) {
                     if (!err) {
-                        var signindetail = JSON.parse(JSON.stringify(result));
+                        var signindetail = JSON.tryParse(JSON.stringify(result));
                         signindetail.forEach(function(s) {
                             s.time = (new Date(s.time)).toLocaleString('zh-CN', { hour12 : false })
                                 .replace(/[\/|-]/, '年').replace(/[\/|-]/, '月').replace(/ /, '日 ');
@@ -102,7 +113,21 @@ router.post('/editcourse', function(req, res, next) {
         var params = url.parse(req.url, true).query;
         dao.updatecourse(req.query.id, req.body.form_coursename, req.body.form_coursetime, req.body.form_courseinfo, function(err, result){
             if (!err) {
-                res.render('home/reload', { location : 'course' });
+                dao.delstuofcourse(req.query.id, function(err, result){
+                    if (!err) {
+                        dao.addstutocourse(req.query.id, JSON.tryParse(req.body.students), function(err, result){
+                            if (!err) {
+                                res.render('home/reload', { location : 'course' });
+                            }
+                            else {
+                                res.render('error', { error : err });
+                            }
+                        });
+                    }
+                    else {
+                        res.render('error', { error : err });
+                    }
+                });      
             } else {
                 res.render('error', { error : err });
             }
@@ -142,11 +167,11 @@ router.get('/coursedetail', function(req, res, next) {
             if (!err) {
                 dao.getcoursebyid(params.id, function(err, result){
                     if (!err) {
-                        var coursedetail = JSON.parse(JSON.stringify(result))[0];
+                        var coursedetail = JSON.tryParse(JSON.stringify(result))[0];
                         coursedetail.course_id = params.id;
                         dao.getstubycourse(params.id, function(err, studentresult){
                             if (!err) {
-                                var student = JSON.parse(JSON.stringify(studentresult));
+                                var student = JSON.tryParse(JSON.stringify(studentresult));
                                 coursedetail.students = student;
                                 res.render('home/coursedetail', coursedetail);
                             }
