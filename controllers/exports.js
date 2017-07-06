a4ba59eec8009a951b45707802deea0e3b3a80b2
zuozhuanz <wenxr3@mail2.sysu.edu.cn>
2017-07-06 16:28:57 +0800

    增加导出数据的统计

--- controllers/exports.js
+++ controllers/exports.js
@@ -62,18 +62,70 @@ function exportall(course_id) {
                 if (t.score == null) {
                     t.score = 0;
                 }
-                examsheet[exam[t.exam_id] + student[t.stu_id]] = {v: t.score};
+                examsheet[exam[t.exam_id] + student[t.stu_id]] = {'v': t.score, 't': 'n'};
             }
             return Promise.resolve();
         });
         return Promise.all([p4, p5]);
     }).then(result => {
-        signsheet['!ref'] = "A1:" + num2char(sign.length+2) + (student.length+2);
+        var signcol = [
+            {wch:10},
+            {wch:10}
+        ]
+        for (var i = 0; i < sign.length; ++i) {
+            signcol.push({wch: 20});
+        }
+        signcol.push({wch: 10});
+
+        var examcol = [
+            {wch:10},
+            {wch:10}
+        ]
+        for (var i = 0; i < exam.length; ++i) {
+            examcol.push({wch: 10});
+        }
+        examcol.push({wch: 10});
+
+        signsheet['!ref'] = "A1:" + num2char(sign.length+3) + (student.length+2);
+        signsheet['!cols'] = signcol;
+
         signsheet['A1'] = {v: '学号'};
         signsheet['B1'] = {v: '姓名'};
-        examsheet['!ref'] = "A1:" + num2char(exam.length+2) + (student.length+2);
+        signsheet['B'+(student.length+2)] = {v: '统计'}
+        signsheet[num2char(sign.length+3)+1] = {v: '统计'}
+
+        examsheet['!ref'] = "A1:" + num2char(exam.length+3) + (student.length+2);
+        examsheet['!cols'] = examcol;
         examsheet['A1'] = {v: '学号'};
         examsheet['B1'] = {v: '姓名'};
+        examsheet['B'+(student.length+2)] = {v: '平均分'}
+        examsheet[num2char(exam.length+3)+1] = {v: '平均分'}
+        // 按次统计
+        for (var i = 0; i < sign.length; ++i) {
+            signsheet[num2char(i+3)+(student.length+2)] = {
+                f: 'COUNTBLANK(' + num2char(i+3) + '2:' + num2char(i+3) + (student.length+1) + ')'
+            }
+        }
+
+        for (var i = 0; i <exam.length; ++i) {
+            examsheet[num2char(i+3)+(student.length+2)] = {
+                f: 'AVERAGE(' + num2char(i+3) + '2:' + num2char(i+3) + (student.length+1) + ')'
+            }
+        }
+
+        // 按学生统计
+        for (var i = 0; i < student.length; ++i) {
+            signsheet[num2char(sign.length+3)+(i+2)] = {
+                f: 'COUNTBLANK(C' +(i+2) + ':' + num2char(sign.length+2) + (i+2) + ')'
+            }
+        }
+
+        for (var i = 0; i < student.length; ++i) {
+            examsheet[num2char(exam.length+3)+(i+2)] = {
+                f: 'AVERAGE(C' +(i+2) + ':' + num2char(exam.length+2) + (i+2) + ')'
+            }
+        }
+
         var workbook = {
             SheetNames: ['签到情况', '测验情况'],
             Sheets: {
